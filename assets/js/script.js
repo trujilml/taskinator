0e6ccdda1b0d4b9964454580f226aeaf3dc77309
trujilml <mickeytrj@gmail.com>
2021-10-06 11:21:19 -0500

    allowed edit task to save

--- assets/js/script.js
+++ assets/js/script.js
@@ -58,14 +58,35 @@ var taskFormHandler = function(event) {
 
     formEl.reset();
 
-    //package up data as an object
+    var isEdit = formEl.hasAttribute("data-task-id");
+    
+    //has data attribute so get task id and call function to complete edit process
+    if (isEdit) {
+        var taskId = formEl.getAttribute("data-task-id");
+        completeEditTask(taskNameInput, taskTypeInput, taskId);
+    }
+    
+    //no data attribute so create object as normal and pass to createTaskEl function
+    else {
     var taskDataObj = {
         name: taskNameInput,
         type: taskTypeInput
     };
 
-    //send it as an argument to createTaskEl
     createTaskEl(taskDataObj);
+}
+};
+
+var completeEditTask = function(taskName, taskType, taskId) {
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+
+    //set new values
+    taskSelected.querySelector("h3.task-name").textContent = taskName;
+    taskSelected.querySelector("span.task-type").textContent = taskType;
+
+    alert("Task Updated!");
+    formEl.removeAttribute("data-task-id");
+    document.querySelector("#save-task").textContent = "Add Task";
 };
 
 var createTaskEl = function(taskDataObj) {
